27874a96d61c10363ae4bca31dd9ec2c9100c876
pds54 <35676624+pds54@users.noreply.github.com>
2018-04-17 16:18:04 -0500

    Update mycd.c

--- mycd/mycd.c
+++ mycd/mycd.c
@@ -24,14 +24,14 @@ void mycd(char *argument) {
   char *tempPath; //temporary path holder
   char *token = strtok(argument, "/"); //delimiter for argument path
   int count = 0; //generic count
-  int oldCount = 0;
-  int size = 0;
+  int oldCount = 0; //count for old path
+  int size = 0; //size of path
   int i = 0; //generic count
   int j = 0; //generic count
   char **newPath = (char **) malloc(sizeof(char *) * (count + 1)); //allocate size for argument
-  char **oldPath = (char **) malloc(sizeof(char *) * (oldCount + 1));  
+  char **oldPath = (char **) malloc(sizeof(char *) * (oldCount + 1));  //allocate size for old path
 
-  if(delimiterCheck != NULL) {
+  if(delimiterCheck != NULL) { //if argument has the delimiter
     while(token != NULL) { //while delimiter still exists
       newPath[count] = malloc(sizeof(char) * ((strlen(token) + 1))); //allocate size for delimiter string
       strcpy(newPath[count], token); //copy string into array
@@ -47,68 +47,68 @@ void mycd(char *argument) {
       }
     }
   }
-  else {
+  else { //if argument does not have delimiter
     newPath[count] = argument;
     printf("%s\n", newPath[count]); //array check
     count++;
   }  
 
-  while(i < count) {
-    if(strcmp(newPath[i], ".") == 0) {
-      if(setenv("NEWPATH", currentPath, 0) == -1) {
+  while(i < count) { //while going through array
+    if(strcmp(newPath[i], ".") == 0) { //if argument is .
+      if(setenv("NEWPATH", currentPath, 0) == -1) { //check that setting environment does not fail
         perror("Error");
         exit(1);
       }
-    currentPath = getenv("NEWPATH");
-    printf("New Path: %s\n", currentPath);
+    currentPath = getenv("NEWPATH"); //make current path new path
+    printf("New Path: %s\n", currentPath); //check path is correct
     }
-    else if(strcmp(newPath[i], "..") == 0) {
-      delimiterCheck = strstr(currentPath, "/");
+    else if(strcmp(newPath[i], "..") == 0) { //if path is ..
+      delimiterCheck = strstr(currentPath, "/"); //check if there is a delimiter
       token = strtok(currentPath, "/");
 
-      if(delimiterCheck != NULL) {
-        while(token != NULL) {
-          oldPath[oldCount] = malloc(sizeof(char) * ((strlen(token) + 1)));
-          size = size + strlen(token) + 1;
+      if(delimiterCheck != NULL) { //if there is a delimiter
+        while(token != NULL) { //while delimiter still exists
+          oldPath[oldCount] = malloc(sizeof(char) * ((strlen(token) + 1))); //allocate size for array
+          size = size + strlen(token) + 1; //keep size of path
           strcpy(oldPath[oldCount], token);
           oldCount++;
           token = strtok(NULL, "/");
 
-          if(token != NULL) {
-            char **resize = (char **) realloc(oldPath, sizeof(char *) * (oldCount + 1));
+          if(token != NULL) { //if delimiter still exists
+            char **resize = (char **) realloc(oldPath, sizeof(char *) * (oldCount + 1)); //resize array
 
-            if(resize != NULL)
-              oldPath = resize;
+            if(resize != NULL) //if resize does not fail
+              oldPath = resize; //resize
           } 
         }
 printf("Before temp alloccation\n"); 
-        tempPath = malloc(size);
+        tempPath = malloc(size); //allocate temporary array to size of old path
 printf("Size: %d\n", sizeof(tempPath));
         tempPath = "";
 printf("Before loop\n");
         while(j < oldCount - 1) {
-          strncat(tempPath, oldPath[j], strlen(oldPath[j]));
+          strncat(tempPath, oldPath[j], strlen(oldPath[j])); //ERROR: Segmentation fault
 printf("In loop %d\n", j);
 printf("String: %s\n", oldPath[j]);
           if(j < oldCount - 2)
-            strcat(tempPath, "/");
+            strcat(tempPath, "/"); //add delimiter if not at the end
           j++;
         }
         oldCount--;
-        currentPath = tempPath;
+        currentPath = tempPath; //make temporary path new path
       
-        if(setenv("NEWPATH", currentPath, 1) == -1) {
+        if(setenv("NEWPATH", currentPath, 1) == -1) { //check if setting environment fails
           perror("Error");
           exit(1);
         }   
       }
-      else {
+      else { //keep path the same if at top directory
         if(setenv("NEWPATH", currentPath, 0) == -1) {
           perror("Error");
           exit(1);
         }
-        currentPath = getenv("NEWPATH");
-        printf("New Path %s\n", currentPath);
+        currentPath = getenv("NEWPATH"); //get new path
+        printf("New Path %s\n", currentPath); //check if path is correct
       }
     }
     i++;
