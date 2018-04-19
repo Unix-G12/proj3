6f6a66ae7c3f514b84c94ed9a2a803a911e41917
pds54 <35676624+pds54@users.noreply.github.com>
2018-04-19 14:58:18 -0500

    Update sexy_shell.c

--- mysexyshell/sexy_shell.c
+++ mysexyshell/sexy_shell.c
@@ -50,6 +50,25 @@ void myls() {
 
 }
 
-void mycd() {
+void mycd(char* argument) {
+  if(argument == NULL) {
+    printf("Expected argument\n");
+    exit(1);
+  }
+  
+  const int MAX = 4096;
+  char path[MAX];
+  char cwd[MAX + 1];
 
+  strcpy(path, argument);
+
+  if(path[0] != '/') {
+    getcwd(cwd, sizeof(cwd));
+    strcat(cwd, "/");
+    strcat(cwd, path);
+    chdir(cwd);
+  }
+  else
+    chdir(path);
+  exit(0);
 }
